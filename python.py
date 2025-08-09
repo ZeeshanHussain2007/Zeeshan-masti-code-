f3cb333e5c83dde86cc5c97d1660260799f3652e
Zeeshan <zeeshanhussain83660@gmail.com>
2025-08-09 18:47:34 +0530

    Create mojmasticode.py

---
+++ python.py
@@ -0,0 +1,33 @@
+name = input("enter your name: ")
+
+nizamuddin = ("nizam wo beautiful isaan hai jis ne irfan ki gand ek nai do nai theen nai ∞ baar maari hai oor khoon 🩸 nikle tak maari or irfan rone laga😭😭 itna roya😭 itna roya😭 ke bihar se bande aagaya irfan ko uthane ke liye ohh bhai sahab🤣🤣🤣")
+
+haseeb = ("haseeb toh chutiya hai uske kali ata ke niche rahata bhona chirkut saala🤏")
+
+ataurrhaman = ("ab aate hai ata mota bail ka bulla hai in ek number ka kali pett hila lete chalta bhai mota or bail ka 5 wa bhi hai in toh🤣🤣")
+
+irfan = ("arre irfan ka kya bolu yaaro usku toh bande leke gya hua hai na khoon 🩸 nikle baad bihar ku pad pina ne🩹🩹")
+
+zeeshan = ("arre inke baare mein maat pucho ji ino toh model hai model haan😎 or inki begum hai nizamiya😘")
+
+if("nizam wo beautiful isaan hai jis ne irfan ki gand ek nai do nai theen nai ∞ baar maari hai oor khoon nikle tak maari or irfan rone laga😭😭 itna roya😭 itna roya😭 ke bihar se bande aagaya irfan ko uthane ke liye ohh bhai sahab🤣🤣🤣"):
+    print(nizamuddin)
+if("haseeb toh chutiya hai uske kali ata ke niche rahata bhona chirkut saala🤏"):
+    print(haseeb)
+if("ab aate hai ata mota bail ka bulla hai in ek number ka kali pett hila lete chalta bhai mota or bail ka 5 wa bhi hai in toh🤣🤣"):
+    print(ataurrhaman)
+if("arre irfan ka kya bolu yaaro usku toh bande leke gya hua hai na khoon nikle baad bihar ku pad pina ne🩹🩹"):
+    print(irfan)
+if("arre inke baare mein maat pucho ji ino toh model hai model haan😎 or inki begum hai nizamiya😘"):
+    print(zeeshan)
+elif("zeeshan ne dekh kar bhoot maze liye"):
+    print(name)
+else:
+    ("or irfan ko chup karwaya")
+
+
+'''IRFAN BHAI MUJHE MAAF KAR DENA YE CODE LIKH NE KE LIYA 
+
+MAI TOH BAS THODI SI PRACTICE KAR RAHA THA 
+
+TOH SOCHA AAPKE MAZEE LELU BAS 🤣🤣🤣🤣'''
