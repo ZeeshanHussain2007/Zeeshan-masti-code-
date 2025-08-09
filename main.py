f6052f1f0ed6b53bb1e562eaa69b9775a107ca2e
Zeeshan <zeeshanhussain83660@gmail.com>
2025-08-09 19:39:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,20 @@
 name = input("enter your name: ")
 
-nizamuddin = ("nizam wo beautiful isaan hai jis ne irfan ki gand ek nai do nai theen nai ∞ baar maari hai oor khoon 🩸 nikle tak maari or irfan rone laga😭😭 itna roya😭 itna roya😭 ke bihar se bande aagaya irfan ko uthane ke liye ohh bhai sahab🤣🤣🤣")
+nizamuddin = "nizam wo beautiful isaan hai jis ne irfan ki gand ek nai do nai theen nai ∞ baar maari hai oor khoon 🩸 nikle tak maari or irfan rone laga😭😭 itna roya😭 ke bihar se bande aagaya irfan ko uthane ke liye ohh bhai sahab🤣🤣🤣"
+haseeb = "haseeb toh chutiya hai uske kali ata ke niche rahata bhona chirkut saala🤙"
+ataurrahman = "ab aate hai ata mota bail ka bulla hai in ek number ka kali pett hila lete chalta bhai mota or bail ka 5 wa bhi hai in toh🤣🤣"
+irfan = "arre irfan ka kya bolu yaaro usku toh bande leke gya hua hai na khoon 🩸 nikle baad bihar ku pad pina ne✒️✒️"
+zeeshan = "arre inke baare mein maat pucho ji ino toh model hai model haan😎 or inki begum hai nizamiyya😘"
 
-haseeb = ("haseeb toh chutiya hai uske kali ata ke niche rahata bhona chirkut saala🤏")
-
-ataurrhaman = ("ab aate hai ata mota bail ka bulla hai in ek number ka kali pett hila lete chalta bhai mota or bail ka 5 wa bhi hai in toh🤣🤣")
-
-irfan = ("arre irfan ka kya bolu yaaro usku toh bande leke gya hua hai na khoon 🩸 nikle baad bihar ku pad pina ne🩹🩹")
-
-zeeshan = ("arre inke baare mein maat pucho ji ino toh model hai model haan😎 or inki begum hai nizamiya😘")
-
-if("nizam wo beautiful isaan hai jis ne irfan ki gand ek nai do nai theen nai ∞ baar maari hai oor khoon nikle tak maari or irfan rone laga😭😭 itna roya😭 itna roya😭 ke bihar se bande aagaya irfan ko uthane ke liye ohh bhai sahab🤣🤣🤣"):
+if name.lower() == "nizamuddin":
     print(nizamuddin)
-if("haseeb toh chutiya hai uske kali ata ke niche rahata bhona chirkut saala🤏"):
+elif name.lower() == "haseeb":
     print(haseeb)
-if("ab aate hai ata mota bail ka bulla hai in ek number ka kali pett hila lete chalta bhai mota or bail ka 5 wa bhi hai in toh🤣🤣"):
-    print(ataurrhaman)
-if("arre irfan ka kya bolu yaaro usku toh bande leke gya hua hai na khoon nikle baad bihar ku pad pina ne🩹🩹"):
+elif name.lower() == "ataurrhaman":
+    print(ataurrahman)
+elif name.lower() == "irfan":
     print(irfan)
-if("arre inke baare mein maat pucho ji ino toh model hai model haan😎 or inki begum hai nizamiya😘"):
+elif name.lower() == "zeeshan":
     print(zeeshan)
-elif("zeeshan ne dekh kar bhoot maze liye"):
-    print(name)
 else:
-    ("or irfan ko chup karwaya")
-
-
-'''IRFAN BHAI MUJHE MAAF KAR DENA YE CODE LIKH NE KE LIYA 
-
-MAI TOH BAS THODI SI PRACTICE KAR RAHA THA 
-
-TOH SOCHA AAPKE MAZEE LELU BAS 🤣🤣🤣🤣'''
+    print("Bhai aisa naam list me nahi hai 😏")
